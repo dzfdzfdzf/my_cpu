e69a80a166b1e30a5ede0ca57cd92c390998bec9
whu-dzf <729159553@qq.com>
2022-05-09 15:59:26 +0800

    测试文件修改

--- Test_8_Instr.asm
+++ Test_8_Instr.asm
@@ -6,12 +6,12 @@
 
 .text
 	ori x29, x0, 12
-	ori x8, x0, 0x234
+	ori x8, x0, 0x123
 	ori x9, x0, 0x456
 	add x7, x8, x9
 	sub x6, x7, x9
-    or  x10, x8, x9
-    and x11, x9, x10
+                or  x10, x8, x9
+                and x11, x9, x10
 	sw x8, 0(x0)
 	sw x9, 4(x0)
 	sw x7, 4(x29)
